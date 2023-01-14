3aae1d0c3deab15d9a05729c401c5f8d430c945a
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-14 10:34:12 -0300

    concluindo o projeto

--- 09 - jQuery/exercício_jquery/script.js
+++ 09 - jQuery/exercício_jquery/script.js
@@ -1,45 +1,21 @@
-// capturando elementos do DOM
-const form = document.getElementById('form_tarefas');
-const tasks = [];
+$(document).ready(function() {
+    
+        $('form').on('submit', function(e) {
+            e.preventDefault();
+            const task = $('#tarefa').val();
+            console.log(task);
+            const newTask = $(`<li>${task}</li>`);
+            $(newTask).appendTo(`ul`);
+            $('#tarefa').val('');
 
-// armazernando todas as linhas digitadas em um array glogal
-let lines = [];
+            $('ul.lista_container li').click(function () {
+                const li = $(this).text();
+                $("li").css("text-decoration", "line-through");
+            })
+            
+        })
+    
+    })
 
-form.addEventListener('submit', function(evento) {
-    // deabilitando reload da página
-    evento.preventDefault();
 
-    // funções auxiliares
-    addLine();
-    updateTable();
-});
-
-// função que adiciona uma linha a tabela
-function addLine() {
-    // recuperando os valores dos inputs
-    const inputTasks = document.getElementById('tarefa');
-
-    // validando duplicidade de registros
-    if(tasks.includes(inputTasks.value)) {
-        alert(`A tarefa ${inputTasks.value} já foi cadastrada!`);
-    } else {
-        // guardando os inputs em arrays
-        tasks.push(inputTasks.value);
-
-        // preparando a inserção de dados na tabela
-        let line = '<tr>';
-        line += `<td>${inputTasks.value}</td>`;
-        lines += line;
-    };
-
-    // limpando os campos após o submit
-    inputTasks.value = '';
-};
-
-// função que atualiza a tabela
-function updateTable() {
-    // recuperando o corpo da tabela
-    const tbody = document.querySelector('tbody');
-    // inserindo as linhas na tabela
-    tbody.innerHTML = lines;
-};
+    
